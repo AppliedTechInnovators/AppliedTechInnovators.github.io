e527673f36f194143a2f2fbe61b8422b3a1852f5
Victor Perez <vperezma89@gmail.com>
2015-10-26 18:07:32 -0700

    current changes.

--- javascript/math.js
+++ javascript/math.js
@@ -1,10 +1,6 @@
-/**
- * Created by victor on 4/1/15.
- */
-<script>
-function myFunction() {
-    var y = document.getElementById("some_number").value;
-    var z = document.getElementById("some_other_number").value;
-    var x = +y + +z;
-    document.getElementById("answer").value && console.log(x) = x;
+
+
+function gearRatio(DrivenSprocket,DriverSprocket) {
+
+
 }
